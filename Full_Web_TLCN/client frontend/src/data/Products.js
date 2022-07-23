3345f85a8ef308885542b5b724fff29f03dd190c
HVCuong2272 <80566331+HVCuong2272@users.noreply.github.com>
2022-07-23 12:26:01 +0700

    upload

---
+++ Full_Web_TLCN/client frontend/src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "Abel King Bed",
+    image: "/images/6.png",
+    description:
+    "Great impressions. Mixing mediums with sophisticated ease, this striking collection features mirrored perimeters, silver flake finishes & diamond-shaped button tufting. The angular shapes & profiles add an art-deco feel to each stunning piece.",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Grady Dining Table",
+    image: "/images/5.png",
+    description:
+    "Bathed in a dark, rich walnut finish, the Grady Dining Collection brings newfound sophistication to your family dining space. Highlighted elements include strong block legs, a straightforward profile, and a casual sense of gentle charm. Two 18″ leaves make it so simple to accommodate guests, appointing this piece perfect for holiday gatherings or full family evening meals. The side chairs’ padded seats ensure cozy dining everyone will appreciate.  Includes two 18″ leaves to extend the table from 59″ to 95″",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Avery Dresser",
+    image: "/images/4.png",
+    description:
+    "The Avery collection brings a traditional look to your room and is finished is made up of solid pine wood finished in a beautiful espresso brown. The bed features gorgeous black leather upholstery that is button-tufted for extra flair.  The case pieces feature felt-lined drawers and are made up of French dovetail construction.",
+    price: 699,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Adriana Chest",
+    image: "/images/3.png",
+    description:
+    "The Adriana transitional style bedroom set in gray finish wood brings the influence of European antiques to life with detailed carvings, looping and a gorgeous brushed silver finish on its pieces. The hand applied brush finish enhances the beauty of the detailed woodwork carvings. This beautiful bed features tufted velour upholstery on the headboard. This elaborate bed will surely bring true splendor into your home.",
+    price: 699,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "Granada Chair in Beige",
+    image: "/images/2.png",
+    description:
+    "The Granada Chair is perfect for establishing modern style in your living room. Built on tapered wooden feet, it features a streamlined frame upholstered in beige colored microfiber for a crisp look. Removable cushions with foam fill and a web suspension in the seat team up to offer comfort as you kick back with your latest read or chat with friends after dinner.",
+    price: 249,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Adore Reclining Sofa",
+    image: "/images/1.png",
+    description:
+    "The Adore Reclining collection features a hardwood & furniture grade plywood frame, high waterfall back, full chaise recliner & low pillow top arms. It comes in a beautiful brown color fabric upholstery. This piece will sure give your home a stylish look.",
+    price: 869,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
